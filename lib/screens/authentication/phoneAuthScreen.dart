a534a49e3e37940708be55cc774522bac6eb1bf3
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-08-05 14:37:26 +0500

    Created the basic next button and with the dialog box along with some authentication.

--- lib/screens/authentication/phoneAuthScreen.dart
+++ lib/screens/authentication/phoneAuthScreen.dart
@@ -10,87 +10,145 @@ class PhoneAuthScreen extends StatefulWidget {
 }
 
 class _PhoneAuthScreenState extends State<PhoneAuthScreen> {
+  bool validate = false;
   var countryCodeController = TextEditingController(text: "+92");
   var phoneNumberController = TextEditingController();
 
-  String counterText = '0';
+// Alert Dialog
+  showAlertDialog(BuildContext context) {
+    AlertDialog alert = AlertDialog(
+        content: Row(
+      children: [
+        CircularProgressIndicator(
+          valueColor:
+              AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
+        ),
+        SizedBox(width: 10),
+        Text("Please wait..."),
+      ],
+    ));
+    showDialog(context: context, builder: (BuildContext context) => alert);
+  }
+
+  phoneAuthentication(number) {
+    print(number);
+  }
+
+  // String counterText = '0';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          iconTheme: IconThemeData(
-            color: Colors.orange,
-          ),
-          title: Text(
-            'Login',
-            style: TextStyle(color: Colors.orange),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Colors.orange,
+        ),
+        title: Text(
+          'Login',
+          style: TextStyle(color: Colors.orange),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            CircleAvatar(
+              radius: 30,
+              backgroundColor: Colors.red.shade200,
+              child: Icon(
+                CupertinoIcons.person_alt_circle,
+                color: Colors.red,
+                size: 60,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text('Enter your phone number',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 10,
+            ),
+            Text('We will send a confirmation code on your phone.',
+                style: TextStyle(color: Colors.grey)),
+            Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: TextFormField(
+                      controller: countryCodeController,
+                      enabled: false,
+                      decoration: InputDecoration(
+                          labelText: "Country", counterText: "10"),
+                    )),
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                    flex: 3,
+                    child: TextFormField(
+                      autofocus: true,
+                      maxLength: 10,
+                      keyboardType: TextInputType.phone,
+                      controller: phoneNumberController,
+                      onChanged: (value) {
+                        if (value.length == 10) {
+                          setState(() {
+                            validate = true;
+                          });
+                        }
+                        // ignore: prefer_is_empty
+                        if (value.length < 0) {
+                          setState(() {
+                            validate = false;
+                          });
+                        }
+                      },
+                      enabled: true,
+                      decoration: InputDecoration(
+                          labelText: "Number",
+                          // hintText: 'Enter your phone number',
+
+                          hintStyle:
+                              TextStyle(fontSize: 10, color: Colors.grey)),
+                    )),
+              ],
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: AbsorbPointer(
+            absorbing: validate ? false : true,
+            child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: validate
+                        ? MaterialStateProperty.all(
+                            Theme.of(context).primaryColor)
+                        : MaterialStateProperty.all(Colors.grey)),
+                onPressed: () {
+                  String number =
+                      '${countryCodeController.text}${phoneNumberController.text}';
+                  phoneAuthentication(number);
+                  showAlertDialog(context);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    "Next",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                )),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              CircleAvatar(
-                radius: 30,
-                backgroundColor: Colors.red.shade200,
-                child: Icon(
-                  CupertinoIcons.person_alt_circle,
-                  color: Colors.red,
-                  size: 60,
-                ),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Text('Enter your phone number',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-              SizedBox(
-                height: 10,
-              ),
-              Text('We will send a confirmation code on your phone.',
-                  style: TextStyle(color: Colors.grey)),
-              Row(
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: TextFormField(
-                        controller: countryCodeController,
-                        enabled: false,
-                        decoration: InputDecoration(
-                            labelText: "Country", counterText: "10"),
-                      )),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Expanded(
-                      flex: 3,
-                      child: TextFormField(
-                        autofocus: true,
-                        maxLength: 10,
-                        keyboardType: TextInputType.phone,
-                        controller: phoneNumberController,
-                        // onChanged: (value) {
-                        //   setState(() {
-                        //     counterText = value.length.toString();
-                        //   });
-                        // },
-                        enabled: true,
-                        decoration: InputDecoration(
-                            labelText: "Number",
-                            // hintText: 'Enter your phone number',
-
-                            hintStyle:
-                                TextStyle(fontSize: 10, color: Colors.grey)),
-                      )),
-                ],
-              )
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }

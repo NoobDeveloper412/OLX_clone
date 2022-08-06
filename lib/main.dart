58cee084b85fe63113e756677100443309bf145c
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-08-06 12:58:24 +0500

    Integrated firebase into the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:olx_clone/screens/authentication/phoneAuthScreen.dart';
 import 'package:olx_clone/screens/login_screen.dart';
 import 'package:olx_clone/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 

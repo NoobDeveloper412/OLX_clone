8bf41519daf718001825759cdc1e213cb190a769
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-08-07 16:06:37 +0500

    Implemented the override backbutton to the location scree.

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:olx_clone/screens/login_screen.dart';
 
 class LocationScreen extends StatelessWidget {
@@ -11,15 +9,46 @@ class LocationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut().then((value) {
-                Navigator.pushReplacementNamed(context, LoginScreen.id);
-              });
-            },
-            child: Text('SignOut')),
+    DateTime _lastQuitTime = DateTime.now();
+
+    return WillPopScope(
+      onWillPop: () async {
+        final shouldPop = await showDialog<bool>(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Do you want to go back?'),
+              actionsAlignment: MainAxisAlignment.spaceBetween,
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  },
+                  child: const Text('Yes'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                  },
+                  child: const Text('No'),
+                ),
+              ],
+            );
+          },
+        );
+        return shouldPop!;
+        // return false;
+      },
+      child: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut().then((value) {
+                  Navigator.pushReplacementNamed(context, LoginScreen.id);
+                });
+              },
+              child: Text('SignOut')),
+        ),
       ),
     );
   }
